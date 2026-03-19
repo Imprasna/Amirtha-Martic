b9bd3e51cadc779546173dd823cdcf26aa70116d
Imprasna <prasanna@sibbc.org>
2026-03-19 19:40:38 +0000

    new images were added

--- pages/Gallery.tsx
+++ pages/Gallery.tsx
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { X, ZoomIn, Image as ImageIcon } from 'lucide-react';
 
-// Import all photos from the photos folder
+type GalleryImage = {
+  id: number;
+  src: string;
+  title: string;
+};
+
 import photo01 from '../assets/photos/01.webp';
 import photo02 from '../assets/photos/02.webp';
 import photo03 from '../assets/photos/03.webp';
@@ -25,28 +30,131 @@ import photo18 from '../assets/photos/18.webp';
 import photo19 from '../assets/photos/19.webp';
 import photo20 from '../assets/photos/20.webp';
 
-const IMAGES = [
-  { id: 1, src: photo01, title: 'Campus Life' },
-  { id: 2, src: photo02, title: 'Campus Life' },
-  { id: 3, src: photo03, title: 'Campus Life' },
-  { id: 4, src: photo04, title: 'Campus Life' },
-  { id: 5, src: photo05, title: 'Campus Life' },
-  { id: 6, src: photo06, title: 'Campus Life' },
-  { id: 7, src: photo07, title: 'Campus Life' },
-  { id: 8, src: photo08, title: 'Campus Life' },
-  { id: 9, src: photo09, title: 'Campus Life' },
-  { id: 10, src: photo10, title: 'Campus Life' },
-  { id: 11, src: photo11, title: 'Campus Life' },
-  { id: 12, src: photo12, title: 'Campus Life' },
-  { id: 13, src: photo13, title: 'Campus Life' },
-  { id: 14, src: photo14, title: 'Campus Life' },
-  { id: 15, src: photo15, title: 'Campus Life' },
-  { id: 16, src: photo16, title: 'Campus Life' },
-  { id: 17, src: photo17, title: 'Campus Life' },
-  { id: 18, src: photo18, title: 'Campus Life' },
-  { id: 19, src: photo19, title: 'Campus Life' },
-  { id: 20, src: photo20, title: 'Campus Life' },
-];
+import img_001 from '../assets/photos/img_001.jpg';
+import img_002 from '../assets/photos/img_002.jpg';
+import img_003 from '../assets/photos/img_003.jpg';
+import img_004 from '../assets/photos/img_004.jpg';
+import img_005 from '../assets/photos/img_005.jpg';
+import img_006 from '../assets/photos/img_006.jpg';
+import img_007 from '../assets/photos/img_007.jpg';
+import img_008 from '../assets/photos/img_008.jpg';
+import img_009 from '../assets/photos/img_009.jpg';
+import img_010 from '../assets/photos/img_010.jpg';
+import img_011 from '../assets/photos/img_011.jpg';
+import img_012 from '../assets/photos/img_012.jpg';
+import img_013 from '../assets/photos/img_013.jpg';
+import img_014 from '../assets/photos/img_014.jpg';
+import img_015 from '../assets/photos/img_015.jpg';
+import img_016 from '../assets/photos/img_016.jpg';
+import img_017 from '../assets/photos/img_017.jpg';
+import img_018 from '../assets/photos/img_018.jpg';
+import img_019 from '../assets/photos/img_019.jpg';
+import img_020 from '../assets/photos/img_020.jpg';
+import img_021 from '../assets/photos/img_021.jpg';
+import img_022 from '../assets/photos/img_022.jpg';
+import img_023 from '../assets/photos/img_023.jpg';
+import img_024 from '../assets/photos/img_024.jpg';
+import img_025 from '../assets/photos/img_025.jpg';
+import img_026 from '../assets/photos/img_026.jpg';
+import img_027 from '../assets/photos/img_027.jpg';
+import img_028 from '../assets/photos/img_028.jpg';
+import img_029 from '../assets/photos/img_029.jpg';
+import img_030 from '../assets/photos/img_030.jpg';
+import img_031 from '../assets/photos/img_031.jpg';
+import img_032 from '../assets/photos/img_032.jpg';
+import img_033 from '../assets/photos/img_033.jpg';
+import img_034 from '../assets/photos/img_034.jpg';
+import img_035 from '../assets/photos/img_035.jpg';
+import img_036 from '../assets/photos/img_036.jpg';
+import img_037 from '../assets/photos/img_037.jpg';
+import img_038 from '../assets/photos/img_038.jpg';
+import img_039 from '../assets/photos/img_039.jpg';
+import img_040 from '../assets/photos/img_040.jpg';
+import img_041 from '../assets/photos/img_041.jpg';
+import img_042 from '../assets/photos/img_042.jpg';
+import img_043 from '../assets/photos/img_043.jpg';
+import img_044 from '../assets/photos/img_044.jpg';
+import img_045 from '../assets/photos/img_045.jpg';
+import img_046 from '../assets/photos/img_046.jpg';
+import img_047 from '../assets/photos/img_047.jpg';
+import img_048 from '../assets/photos/img_048.jpg';
+import img_049 from '../assets/photos/img_049.jpg';
+
+const IMAGES: GalleryImage[] = [
+  photo01,
+  photo02,
+  photo03,
+  photo04,
+  photo05,
+  photo06,
+  photo07,
+  photo08,
+  photo09,
+  photo10,
+  photo11,
+  photo12,
+  photo13,
+  photo14,
+  photo15,
+  photo16,
+  photo17,
+  photo18,
+  photo19,
+  photo20,
+  img_001,
+  img_002,
+  img_003,
+  img_004,
+  img_005,
+  img_006,
+  img_007,
+  img_008,
+  img_009,
+  img_010,
+  img_011,
+  img_012,
+  img_013,
+  img_014,
+  img_015,
+  img_016,
+  img_017,
+  img_018,
+  img_019,
+  img_020,
+  img_021,
+  img_022,
+  img_023,
+  img_024,
+  img_025,
+  img_026,
+  img_027,
+  img_028,
+  img_029,
+  img_030,
+  img_031,
+  img_032,
+  img_033,
+  img_034,
+  img_035,
+  img_036,
+  img_037,
+  img_038,
+  img_039,
+  img_040,
+  img_041,
+  img_042,
+  img_043,
+  img_044,
+  img_045,
+  img_046,
+  img_047,
+  img_048,
+  img_049,
+].map((src, index) => ({
+  id: index + 1,
+  src,
+  title: 'Campus Life',
+}));
 
 const Gallery: React.FC = () => {
   const [selectedImage, setSelectedImage] = useState<typeof IMAGES[0] | null>(null);
